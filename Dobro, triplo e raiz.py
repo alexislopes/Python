cafaee85d2428b5e706197d7ebdeab146219f222
Alexis Lopes <alexis.m753@gmail.com>
2017-08-02 16:07:25 -0300

    Create Dobro, triplo e raiz.py

---
+++ Dobro, triplo e raiz.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número
+# e mostre seu dobro, triplo e raiz quadrada.
+
+num = int(input('Digite um número: '))
+print('O dobro de {} é: {}'.format(num, num*2))
+print('O triplo de  {} é: {}'.format(num, num*3))
+print('A raiz quadrada de {} é: {}'.format(num, num*(1/2)))
