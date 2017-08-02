80965f1a8a93b9bccba20e7a73eeaed35a991e16
Alexis Lopes <alexis.m753@gmail.com>
2017-08-02 16:01:11 -0300

    Create Sucessor e Antecessor.py

---
+++ Sucessor e Antecessor.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número inteiro
+# e mostre na tela o seu sucessor e seu antecessor.
+
+num = int(input('Digite um número: '))
+print('Sucessor {}\nAntecessor {}'.format(num+1, num-1))
