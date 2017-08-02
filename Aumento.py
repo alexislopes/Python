e7a283a0ac957cac45c6e09654416c94fc3e907a
Alexis Lopes <alexis.m753@gmail.com>
2017-08-02 17:55:16 -0300

    Create Aumento.py

---
+++ Aumento.py
@@ -0,0 +1,7 @@
+# Faça um algoritmo que leia o salário de um funcionário
+# e mostre seu novo salário com 15% de aumento
+
+salario = float(input('Qual seu salário? '))
+aumento = salario + (salario*0.15)
+
+print('Novo salário com 15% de aumento é: {}'.format(aumento))
