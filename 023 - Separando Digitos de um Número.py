5ea6261141d6377ab2b2c9c6c6906d404e94c933
Alexis Lopes <alexis.m753@gmail.com>
2017-08-03 19:23:43 -0300

    Create 023 - Separando Digitos de um Número.py

---
+++ 023 - Separando Digitos de um Número.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia um número de 0 a 9999
+# e mostre na tela cada um dos digitos separados
+
+# EX: Digite um número: 1834
+# Unidade: 4. Dezena: 3. Centena: 8. Milhar: 1.
+
+num = int(input('Digite um número: '))
+u = num//1 %10
+d = num//10 %10
+c = num//100 %10
+m = num//1000 %10
+
+print('Unidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}'.format(u, d, c, m))
